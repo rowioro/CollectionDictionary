1ded6e8ea23647faa80d99a8ec2878b86d1218e1
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 18:44:28 +0200

    Add project files.

---
+++ ConsoleApp11_Kolekcje-Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11_Kolekcje_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "Anna");
+            dictionary.Add(4, "Jozef");
+            dictionary.Add(3, "Maria");
+            if (!dictionary.ContainsKey(2))
+                dictionary.Add(2, "Robert");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0}:{1}",item.Key,item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
